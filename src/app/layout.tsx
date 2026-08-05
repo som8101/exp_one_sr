3c7c5d9fb140d527e2746ce18dccf2ab259b551c
Somnath <somnathgorai0001@gmail.com>
2026-08-05 22:24:40 +0530

    chore: add google adsense script for verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/shared/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,14 @@ export default function RootLayout({
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
+        <head>
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3835252173185326"
+            crossOrigin="anonymous"
+            strategy="afterInteractive"
+          />
+        </head>
         <body suppressHydrationWarning className="min-h-full flex flex-col">
           <ThemeProvider
             attribute="class"
